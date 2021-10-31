0c535d4b5479316e5c676bdb605f17c8c4f7e650
RajDineshJagasia <rj2011@hw.ac.uk>
2021-10-31 15:09:38 +0400

    finally got the bees moving

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
-var bear ;
-var bees;
+
 
 
 function Bear() {
@@ -51,6 +50,7 @@ function start() {
   bees = new Array();
   //create bees
   makeBees();
+  updateBees();
 
 }
 
@@ -186,22 +186,20 @@ function moveBees() {
   let speed = document.getElementById("speedBees").value;
   //move each bee to a random location
   for (let i = 0; i < bees.length; i++) {
-    let dx = getRandomInt(2 * speed) - speed;
-    let dy = getRandomInt(2 * speed) - speed;
-    bees[i].move(dx, dy);
-    isHit(bees[i], bear); //we add this to count stings
+  let dx = getRandomInt(2 * speed) - speed;
+  let dy = getRandomInt(2 * speed) - speed;
+  bees[i].move(dx, dy);
   }
-}
-
-function updateBees() {
-  // update loop for game
+ }
+ 
+ function updateBees() { // update loop for game
   //move the bees randomly
   moveBees();
   //use a fixed update period
-  let period = 10; //modify this to control refresh period
+  let period = document.getElementById("periodTimer").value;//modify this to control refresh period
   //update the timer for the next move
-  updateTimer = setTimeout("updateBees()", period);
-}
+  updateTimer = setTimeout('updateBees()', period);
+ }
 
 function isHit(defender, offender) {
   if (overlap(defender, offender)) {
